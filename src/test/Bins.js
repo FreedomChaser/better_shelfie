39ff24e1a711b0515cceb91d675a2509ab8e6889
FreedomChaser <horsecrazyshellie@gmail.com>
2018-08-10 10:31:16 -0600

    conditional rendering sovled

--- src/test/Bins.js
+++ src/test/Bins.js
@@ -18,50 +18,35 @@ export default class Bins extends Component{
     componentDidMount(){
         axios.get('/api/shelves/' + this.props.id)
         .then(res => {
-            this.setState({bins: res.data})
+            // console.log('res', res)
+            const newBins = []
+            let bind = res.data
+            for(let i=1; i<=5; i++){
+                let match =bind.filter(bin => {
+                    return bin.bin_number === i
+                })
+
+                if(match[0]){
+                    newBins.push(match[0])
+                }else{
+                    newBins.push(null)
+                }
+            }
+            this.setState({bins: newBins})
         })
     }
 
        
-    //        for(i=0; i<6; i++){
-    //            //do a get request
-    //            //if get returns null dipsplay add bid with a link (through routes.hstory.push) to create bin comp
-    //            //if get returns data display bin title and  link to bin display comp
-    //             if(axios.get(`/api/shelves/${this.props.match.params.id}/binId`===null){
-    //                 <Link to=`/api/shelves/`>+ Add Inventory</Link>
-    //             })
-    //        }
-    //    }
     render(){ 
     // console.log(this.props)
-        console.log(this.state.bins)
-        // for(let i = 0; i<6; i++){
-        //     // let newBins = this.state.map.bins(e => {
-        //     //     if()
-        //     // })
-        //     let newBins = []
-        //     if(this.state.bins){
-        //         newBins.push(i)
-        //     }else{
-        //         newBins.push(null)
-        //     }
-        //     //look in bins array
-        //     //if a bin exists display button on click and bin name
-        //     //if bin doesn't exist add a button that says add inventory
-        // }
-        // let nullBin = Array(5).fill(null).map((e, i) => {
-        //     if(bins.includes(i+1)){
-        //         return i+1
-        //     }else{
-        //         return null
-        //     }
-        // })
+        // console.log('state', this.state.bins)
+        
         let bin = this.state.bins.map(e => {
-            if(e.product_name && e.price){
-                return <button onClick={()=>this.props.history.push(`/bins/${e.shelf_id}/displayBin/${e.bin_number}`)}>Bin {e.bin_number}</button>
+            if(e === null){
+                return <button onClick={()=>this.props.history.push(`/bins/${e.shelf_id}/addBin/${e.bin_number}`)}>+ Add Inventory</button>
                 // <Link to=''>{e.bin_number}</Link>
             }else{
-                return <button onClick={()=>this.props.history.push(`/bins/${e.shelf_id}/addBin/${e.bin_number}`)}>+ Add Inventory</button>
+                return <button onClick={()=>this.props.history.push(`/bins/${e.shelf_id}/displayBin/${e.bin_number}`)}>Bin {e.bin_number}</button>
             }
         })
        return(
